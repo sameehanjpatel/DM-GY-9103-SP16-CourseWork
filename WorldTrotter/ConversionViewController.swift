8d6e7bc99134ccd17ccf81f38f919379fa560140
sameehanjpatel <sameehanjpatel@gmail.com>
2016-02-15 16:24:33 -0600

    Chapter 4

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -20,12 +20,14 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         return nf
     }()
     
+
     var fahrenheitValue: Double? {
         didSet {
             updateCelsiusLabel()
         }
     }
     
+
     var celsiusValue: Double?{
         if let value = fahrenheitValue {
         return (value - 32) * (5/9)
@@ -53,7 +55,23 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func dismissKeyboard(sender:AnyObject){
         textField.resignFirstResponder()
+    
     }
+    
+    func textField(textField: UITextField,
+        shouldChangeCharactersInRange range: NSRange,
+        replacementString string: String) -> Bool {
+            let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+            let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+            
+            if existingTextHasDecimalSeparator != nil &&
+                replacementTextHasDecimalSeparator != nil {
+                    return false
+            }
+            else
+            {return true}
+    }
+    
     
   
     }
