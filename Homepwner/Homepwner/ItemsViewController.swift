1e3cda7abde1762dce8e26bdfba213733bcbb854
sameehanjpatel <sameehanjpatel@gmail.com>
2016-03-01 15:47:19 -0500

    Chapter 11

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -96,6 +96,8 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = 65
     }
     
     
@@ -106,15 +108,15 @@ class ItemsViewController: UITableViewController {
     override func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             // Create an instance of UITableViewCell, with default appearance
-            let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+            let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
             
             // Set the text on the cell with the description of the item
             // that is at the nth index of items, where n = row this cell
             // will appear in on the tableview
             let item = itemStore.allItems[indexPath.row]
-            
-            cell.textLabel?.text = item.name
-            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            cell.nameLabel.text = item.name
+            cell.serialNumberLabel.text = item.serialNumber
+            cell.valueLabel.text = "$\(item.valueInDollars)"
             
             return cell
     }
